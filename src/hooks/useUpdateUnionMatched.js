3cd7fc082dfa6269a8ca215a91306b9e2e1360f7
Parksy-00 <63638539+Parksy-00@users.noreply.github.com>
2021-03-30 14:01:41 +0900

    fix: 검색창을 삭제해도 fileDisplay에 file이 남아있던 문제 수정

--- src/hooks/useUpdateUnionMatched.js
+++ src/hooks/useUpdateUnionMatched.js
@@ -3,16 +3,18 @@ import { useRecoilValue, useSetRecoilState } from "recoil"
 import matchedFiles from '../states/matchedFiles'
 import unionedMatched from "../states/unionedMatch"
 import allFiles from '../states/allFiles'
+import searchBarIDs from "../states/searchBarIDs"
 
 const useUpdateUnionMatched = (searchBarID) => {
     const Files = useRecoilValue(matchedFiles(searchBarID))
     const setUnionedFiles = useSetRecoilState(unionedMatched)
     const all = useRecoilValue(allFiles)
+    const IDs = useRecoilValue(searchBarIDs)
 
     useEffect(() => {
         if(Object.keys(all).length !== 0) {
-            const files = Object.values(all).flat(1)
-            // Object.values(all) -> (파일이름, 파일태그들)객체의 배열이 검색창 수만큼 있는 배열
+            const keys = Object.keys(all).filter((key) => IDs.includes(parseInt(key)))
+            const files = keys.map((key) => all[key]).flat(1)
 
             let ret = []
             const set = new Set()
