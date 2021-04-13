e76c6840fda50dbed954e07b6421a3d014eb3d9b
yujong-lee <yujonglee@g.skku.edu>
2021-04-13 11:43:30 +0900

    style: ContentFooter

--- src/components/ContentFooter/ContentFooter.js
+++ src/components/ContentFooter/ContentFooter.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React from 'react'
 import 'antd/dist/antd.css'
 import { DownloadOutlined } from '@ant-design/icons'
 import { Typography, List, Button  } from 'antd'
@@ -6,21 +6,21 @@ const Text = Typography.Text
 
 function ContentFooter({selected}) {
 
-    const actions = [<Button type="primary" style={{margin:'10px 10px'}}><DownloadOutlined />다운로드</Button>,
-                    <Button style={{margin:'10px 10px'}}>태그 수정</Button>,
-                    <Button danger style={{margin:"10px 5vw 10px 10px"}}>삭제</Button>]
+    const actions = [
+                        <Button type="primary" style={{margin:'10px 10px'}}><DownloadOutlined />다운로드</Button>,
+                        <Button style={{margin:'10px 10px'}}>태그 수정</Button>,
+                        <Button danger style={{margin:"10px 5vw 10px 10px"}}>삭제</Button>
+                    ]
                      
 
     return (
         <div style={{padding:'20px 40px 10px'}}>
             <div style={{border: '1px solid #d9d9d9', backgroundColor:'white', height: "100%", display:'flex', alignItems:'flex-end'}}>
-                <List.Item
-                    style={{width:'100%', padding:'5px'}}
-                    actions={actions}
-                >
-                    <div>
-                        <Text style={{fontSize:'18px', verticalAlign:'middle', userSelect:'none', marginLeft:'40px'}}>{selected.length} 개의 파일 선택됨</Text>
-                    </div>
+                <List.Item style={{width:'100%', padding:'5px'}}
+                           actions={actions}>
+ 
+                    <Text style={{fontSize:'18px', verticalAlign:'middle', userSelect:'none', marginLeft:'40px'}}>{selected.length} 개의 파일 선택됨</Text>
+ 
                 </List.Item>
                 
             </div>
